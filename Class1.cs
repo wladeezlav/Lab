f3521911e6e0209bea6bdb0cf07bb489f18039aa
wladeezlav <33658671+wladeezlav@users.noreply.github.com>
2018-12-30 01:13:03 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data.Common;
+
+namespace Zalik_Mamontov
+{
+    class Program
+    {
+        static DBworker db = new DBworker();
+        static void Main(string[] args)
+        {
+            GlobalMenu();
+            Console.Read();
+        }
+
+
+        private static void GlobalMenu()
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.Clear();
+                Console.WriteLine("Vladylav Mamontov, IS-6315");
+                Console.WriteLine("Tables:");
+                int last = 0;
+                for (int i = 0; i < db.Tables.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {db.Tables[i]}");
+                    last = i;
+                }
+                Console.WriteLine("4. Tables operations");
+                Console.WriteLine("5. Exit");
+                int index;
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out index) || index < 1 || index > db.Tables.Length + 2)
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input number isn't correct");
+                    Console.WriteLine();
+                }
+                else if (index == 4)
+                {
+                    db.Queries();
+                    repeat = true;
+                }
+                else if (index == 3)
+                {
+                    db.Show(index - 1);
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    repeat = true;
+                    Console.Clear();
+                }
+                else if (index == 5)
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    TableMenu(index - 1);
+                    Console.Clear();
+                    repeat = true;
+                }
+            }
+            Console.WriteLine("Thank you!");
+
+        }
+
+        private static void TableMenu(int tableNum)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                db.Show(tableNum);
+                Console.WriteLine(new string('_', 50));
+
+                Console.WriteLine("Select function:");
+                Console.WriteLine("1. Add new row");
+                Console.WriteLine("2. Delete row");
+                Console.WriteLine("3. Edit row");
+                Console.WriteLine("4. Back to previous menu");
+
+                int index;
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out index))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input number isn't corcect");
+                    Console.WriteLine();
+
+                }
+                else
+                {
+                    switch (index)
+                    {
+                        case 1:
+                            {
+                                repeat = true;
+                                Add(tableNum);
+                                Console.Clear();
+                                break;
+                            }
+                        case 2:
+                            {
+                                repeat = true;
+                                Delete(tableNum);
+                                Console.Clear();
+                                break;
+                            }
+                        case 3:
+                            {
+                                repeat = true;
+                                Edit(tableNum);
+                                Console.Clear();
+                                break;
+                            }
+                        case 4:
+                            {
+                                repeat = false;
+                                //GlobalMenu();
+                                break;
+                            }
+                        default:
+                            {
+                                repeat = true;
+                                Console.WriteLine();
+                                Console.WriteLine("Input less than 1 or more than 4");
+                                Console.WriteLine();
+                                break;
+                            }
+                    }
+                }
+
+            }
+
+        }
+
+        private static void Add(int tableNum)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Enter new values in one string\n### dividing values using symbol ','\n### enter all columns except table ID");
+
+                string input = Console.ReadLine();
+
+                if (!db.Add(tableNum, input))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input data isn't correct. Try again");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    repeat = false;
+                }
+
+            }
+        }
+
+        private static void Delete(int tableNum)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Select No. of row, which will be deleted or 0 if table doesn't have any rows");
+
+                int rowNum;
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out rowNum))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input number isn't corcect");
+                    Console.WriteLine();
+
+                }
+                else if (!db.Delete(tableNum, rowNum - 1))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input number less than 0 or more than latest row number");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    repeat = false;
+                }
+
+            }
+        }
+
+        private static void Edit(int tableNum)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Select No. of row, which will be edit or 0 if table is empty");
+
+                int rowNum;
+                string input1 = Console.ReadLine();
+
+                Console.WriteLine("Enter new values in one string\n### dividing values using symbol ','\n### enter all columns except table ID");
+
+                string input2 = Console.ReadLine();
+
+
+                if (!int.TryParse(input1, out rowNum))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input number isn't corcect");
+                    Console.WriteLine();
+
+                }
+                else if (!db.Edit(tableNum, rowNum - 1, input2))
+                {
+                    repeat = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Input data isn't correct. Try again");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    repeat = false;
+                }
+
+            }
+        }
+
+    }
+
+
+}
---
+++ DBworker.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data.Common;
+
+
+namespace Zalik_Mamontov
+{
+    class DBworker
+    {
+        public static string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Учёба\C#\Zalik\Zalik_Mamontov\Zalik_Mamontov\Zalik.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public SqlConnection connection = new SqlConnection(DBworker.conn);
+       
+        DataSet ds = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+
+        public string[] Tables { get; private set; } = new string[] { "Actor", "Film", "Acting" };
+
+
+        public bool Add(int tableNum, string row)
+        {
+            
+                connection.Open();
+                
+                adapter.SelectCommand = new SqlCommand(GetSQL(tableNum), connection);
+                adapter.Fill(ds);
+
+                DataRow newRow;
+
+                switch (tableNum)
+                {
+                    case 0:
+                        {
+                            newRow = AddActor(row, ds.Tables[0].NewRow());
+                            if (newRow == null)
+                            {
+                                connection.Close();
+                                return false;
+                            }
+                            break;
+                        }
+                    case 1:
+                        {
+                            newRow = AddFilm(row, ds.Tables[0].NewRow());
+                            if (newRow == null)
+                            {
+                                connection.Close();
+                                return false;
+                            }
+
+                            break;
+                        }
+                    default:
+                        {
+                            connection.Close();
+                            return false;
+
+                        }
+                }
+
+
+                ds.Tables[0].Rows.Add(newRow);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Update(ds);
+                connection.Close();
+            
+            return true;
+        }
+
+        private DataRow AddActor(string row, DataRow newRow)
+        {
+            string[] rowData = row.Split(',');
+            string Full_name;
+            DateTime DOB;
+
+
+            if (rowData.Length < 2)
+            {
+                return null;
+            }
+
+            Full_name = rowData[0].ToString();
+            if(!DateTime.TryParse(rowData[1], out DOB))
+            {
+                Console.WriteLine("Incorrect date.");
+                return null;
+            }
+
+            newRow["Full_name"] = Full_name;
+            newRow["DOB"] = DOB;
+            adapter.Update(ds);
+
+            return newRow;
+
+        }
+
+        private DataRow AddFilm(string row, DataRow newRow)
+        {
+            string[] rowData = row.Split(',');
+            string title;
+            DateTime date;
+            string country;
+
+            if (rowData.Length < 3)
+            {
+                return null;
+            }
+
+            title = rowData[0].ToString();
+            if (!DateTime.TryParse(rowData[1], out date))
+            {
+                Console.WriteLine("Incorrect date.");
+                return null;
+            }
+            country = rowData[2].ToString();          
+
+            newRow["Title"] = title;
+            newRow["Date"] = date;
+            newRow["Country"] = country;
+            
+            return newRow;
+        }
+
+        public void Show(int tableNum)
+        {
+            const int size = -30;
+
+            //using (SqlConnection connection = conn())
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(GetSQL(tableNum), connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                Console.Write("No.\t");
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader.GetName(i),size}");
+                }
+                Console.WriteLine();
+
+                if (reader.HasRows)
+                {
+
+                    int index = 0;
+                    while (reader.Read())
+                    {
+                        index++;
+                        Console.Write(index + "\t");
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetValue(i),size}");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Table is empty");
+                }
+                connection.Close();
+            }
+        }
+
+        public bool Delete(int tableNum, int rowIndex)
+        {
+            //using (SqlConnection connection = conn())
+            {
+                connection.Open();
+
+                adapter.SelectCommand = new SqlCommand(GetSQL(tableNum), connection);
+                adapter.Fill(ds);
+
+                DataTable dt = ds.Tables[0];
+
+                if (dt.Rows.Count <= rowIndex)
+                {
+                    connection.Close();
+
+                    if (dt.Rows.Count == 0)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+                
+                dt.Rows[rowIndex].Delete();
+
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Update(ds);
+                connection.Close();
+            }
+
+
+            return true;
+        }
+
+        public bool Edit(int tableNum, int rowIndex, string row)
+        {
+            //using (SqlConnection connection = conn)
+            {
+                connection.Open();
+
+                adapter.SelectCommand = new SqlCommand(GetSQL(tableNum), connection);
+                adapter.Fill(ds);
+
+                if(rowIndex >= ds.Tables[0].Rows.Count)
+                {
+                    connection.Close();
+
+                    if (ds.Tables[0].Rows.Count == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                
+                DataRow editRow = ds.Tables[0].Rows[rowIndex];
+
+                switch (tableNum)
+                {
+                    case 0:
+                        {
+                            
+                            if (AddActor(row, editRow) == null)
+                            {
+                                connection.Close();
+                                return false;
+                            }
+                            break;
+                        }
+                    case 1:
+                        {
+                            if (AddFilm(row, editRow) == null)
+                            {
+                                connection.Close();
+                                return false;
+                            }
+
+                            break;
+                        }
+                    default:
+                        {
+                            connection.Close();
+                            return false;
+
+                        }
+                }
+
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Update(ds);
+                connection.Close();
+            }
+            return true;
+        }
+
+        private string GetSQL(int tableNum)
+        {
+            if (tableNum < 0 || tableNum >= Tables.Length)
+            {
+                throw new Exception("Number of table is not correct");
+            }
+            return "Select * From " + Tables[tableNum];
+        }
+
+        private string GetQuery1()
+        {
+            return "Select * From Film Where Date >= '01.01.2017'";
+        }
+
+        private string GetQuery2()
+        {
+            string film;
+            Console.WriteLine("Введите название фильма из таблицы фильмов");
+            film = Console.ReadLine();
+            return "Select * From Actor Where id = (SELECT actor_id FROM Acting WHERE film_id = (SELECT id FROM Film WHERE Title = '" + film + "'))";
+        }
+
+        private string GetQuery3()
+        {
+            Console.WriteLine("Введите минимальное количество фильмов, в которых сыграл актер/актриса");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return "SELECT Acting.actor_id AS Id, Actor.Full_name, Actor.DOB, COUNT(*) AS Roles FROM dbo.Acting JOIN Actor ON(Acting.actor_id = Actor.Id) GROUP BY Full_name, Actor.DOB, actor_id HAVING COUNT(*) >= " + number;
+        }
+        private string GetQuery4_1(int number)
+        {
+            return "DELETE FROM Acting WHERE film_id IN (SELECT id FROM Film WHERE Date < (DATEADD(YEAR, " + number + ", GETDATE())))";
+        }
+        private string GetQuery4_2(int number)
+        {
+            return "DELETE FROM Film WHERE Date<(DATEADD(YEAR, " + number + ", GETDATE()));";
+        }
+        /*  */
+
+        public void Queries()
+        {
+            connection.Open();
+
+            const int size = -30;
+
+            Console.WriteLine("1. Найти все фильмы, вышедшие на экран в прошлом и текущем году.");
+            Console.WriteLine("2. Вывести информацию об актерах, снимавшихся в заданном фильме.");
+            Console.WriteLine("3. Вывести информацию об актерах, снимавшихся как минимум в N фильмах.");
+            Console.WriteLine("4. Удалить все фильмы, дата выхода которых была более заданного числа лет назад.");
+            int key = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            switch (key)
+            {
+                case 0:
+                    {
+                        SqlCommand command = new SqlCommand(GetQuery1(), connection);
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        Console.Write("No.\t");
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetName(i),size}");
+                        }
+                        Console.WriteLine();
+
+                        if (reader.HasRows)
+                        {
+
+                            int index = 0;
+                            while (reader.Read())
+                            {
+                                index++;
+                                Console.Write(index + "\t");
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader.GetValue(i),size}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Query result is empty");
+                        }
+                        connection.Close();
+                        break;
+                    }
+                case 1:
+                    {
+                        SqlCommand command = new SqlCommand(GetQuery2(), connection);
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        Console.Write("No.\t");
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetName(i),size}");
+                        }
+                        Console.WriteLine();
+
+                        if (reader.HasRows)
+                        {
+
+                            int index = 0;
+                            while (reader.Read())
+                            {
+                                index++;
+                                Console.Write(index + "\t");
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader.GetValue(i),size}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Query result is empty");
+                        }
+                        connection.Close();
+                        break;
+                    }
+                case 2:
+                    {
+                        SqlCommand command = new SqlCommand(GetQuery3(), connection);
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        Console.Write("No.\t");
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetName(i),size}");
+                        }
+                        Console.WriteLine();
+
+                        if (reader.HasRows)
+                        {
+
+                            int index = 0;
+                            while (reader.Read())
+                            {
+                                index++;
+                                Console.Write(index + "\t");
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader.GetValue(i),size}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Query result is empty");
+                        }
+                        Console.ReadLine();
+                        connection.Close();
+                        break;
+                    }
+                case 3:
+                    {
+                            Console.WriteLine("Введите количество лет, фильмы старше которого нужно удалить");
+                            int number = -Convert.ToInt32(Console.ReadLine());
+
+                            SqlDataAdapter adapter = new SqlDataAdapter(GetQuery4_1(number), connection);
+                            DataSet ds = new DataSet();
+                            adapter.Fill(ds);
+                            adapter = new SqlDataAdapter(GetQuery4_2(number), connection);
+                            ds = new DataSet();
+                            adapter.Fill(ds);
+                            connection.Close();
+
+                            Console.WriteLine("Completed");
+                            break;
+                        
+                    }
+                default:
+                    {
+                        connection.Close();
+                        return;
+
+                    }
+            }
+        }
+    }
+}
